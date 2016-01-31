1206933d6a11634f9f61d85b1f8b0e7dfbbc3b20
Martin Leonard <mldeepcove@gmail.com>
2016-01-31 10:11:57 -0800

    Initial Commit

---
+++ Imperial Assault Campaign TrackerTests/Imperial_Assault_Campaign_TrackerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Imperial_Assault_Campaign_TrackerTests.swift
+//  Imperial Assault Campaign TrackerTests
+//
+//  Created by Martin Leonard on 2016-01-31.
+//  Copyright © 2016 Martin Leonard. All rights reserved.
+//
+
+import XCTest
+@testable import Imperial_Assault_Campaign_Tracker
+
+class Imperial_Assault_Campaign_TrackerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
