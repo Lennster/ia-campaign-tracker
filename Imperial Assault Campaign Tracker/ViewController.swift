4ee7c452c5965ea7868badfaf4f5c0e18ea3592e
Lennster <Lennster@users.noreply.github.com>
2016-02-06 21:34:01 -0800

    Added photo to initial campaign view.

--- Imperial Assault Campaign Tracker/ViewController.swift
+++ Imperial Assault Campaign Tracker/ViewController.swift
@@ -8,13 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    // MARK: Properties
+    
     @IBOutlet weak var campaignName: UILabel!
     @IBOutlet weak var campaignTextField: UITextField!
+    @IBOutlet weak var campaignImage: UIImageView!
+    @IBOutlet weak var campaignNotesTextView: UITextView!
+    
+    
+    // MARK: Initialization
     
     override func viewDidLoad() {
-        super.viewDidLoad()
+               super.viewDidLoad()
         
         campaignTextField.delegate = self
     }
@@ -24,10 +31,56 @@ class ViewController: UIViewController, UITextFieldDelegate {
         // Dispose of any resources that can be recreated.
     }
 
+    
     // MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        // Hide the keyboard and complete field editing on return key
+        textField.resignFirstResponder()
+        return true
+    }
+    
     func textFieldDidEndEditing(textField: UITextField) {
         print("Entered value \(textField.text)")
         campaignName.text = textField.text
+    }
+    
+    // MARK: UIImagePickerControllerDelegate
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        // Dismiss the picker if the user canceled.
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        // The info dictionary contains multiple representations of the image, and this uses the original.
+        let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        // Set photoImageView to display the selected image.
+        campaignImage.image = selectedImage
+        
+        // Dismiss the picker.
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    // MARK: Actions
+    
+    
+    @IBAction func selectCampaignImageFromLibrary(sender: UITapGestureRecognizer) {
+        //Hide keyboard as we might be in the campaignTextField
+        campaignTextField.resignFirstResponder()
+        
+        // UIImagePickerController is a view controller that lets a user pick media from their photo library.
+        let imagePickerController = UIImagePickerController()
+        
+        // Only allow photos to be picked, not taken.
+        imagePickerController.sourceType = .PhotoLibrary
+        
+        // Make sure ViewController is notified when the user picks an image.
+        imagePickerController.delegate = self
+        
+        presentViewController(imagePickerController, animated: true, completion: nil)
+        
     }
 }
 
